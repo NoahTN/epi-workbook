dc52d3bdc82639e8a06542f4a5cfada2d0747436
NoahTN <ntolentino-nguyen@csumb.edu>
2021-04-14 16:16:02 -0700

    Update reversed words solution

--- 6_strings/6.6_reverse_words.py
+++ 6_strings/6.6_reverse_words.py
@@ -3,25 +3,21 @@
 # ! Strings are immutable in Python so a space complexity O(1) is not possible
 
 class Solution:
-    def reverseWords(self, s: str) -> str:
+   def reverseWords(self, s: str) -> str:
       words = []
-      start = -1
+      i = 0
+      cur = ""
 
-      # Loop through and get the start:end of each word
-      for i in range(len(s)):
-         if s[i] == ' ':
-               continue
-         if start == -1:
-               start = i
-         if i+1 == len(s) or s[i+1] == ' ':
-               words.append((start, i+1))
-               start = -1
+      while i < len(s):
+            if s[i] == ' ':
+                  if cur:
+                        words.append(cur)
+                        cur = ""
+            else:
+                  cur += s[i]
+            i+=1
 
-      # Append each word to result in reverse order         
-      result = ""
-      for i in reversed(range(len(words))):
-         result += s[words[i][0]: words[i][1]]
-         if i > 0:
-               result += ' '
-               
-      return result
+      if cur:
+            words.append(cur)
+
+      return ' '.join(reversed(words))
