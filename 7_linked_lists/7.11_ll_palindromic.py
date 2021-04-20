05cf3d234aee65e5f56986a320245b2727a29a46
NoahTN <ntolentino-nguyen@csumb.edu>
2021-04-20 15:59:34 -0700

    Add final linked list problem

---
+++ 7_linked_lists/7.11_ll_palindromic.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/palindrome-linked-list
+
+
+class Solution:
+   # O(n), O(1)
+   # Modifies the linked list
+   def isPalindrome(self, head: ListNode) -> bool:
+      slow = fast = head
+      while fast and fast.next:
+         fast, slow = fast.next.next, slow.next
+      
+      prev = None
+      while slow:
+         slow.next, prev, slow = prev, slow, slow.next
+         
+      curr = head
+      while prev:
+         if prev.val != curr.val:
+               return False
+         prev = prev.next
+         curr = curr.next
+      
+      return True
+   
+   # O(n), O(n)
+   # Doesn't modify the linked list
+   def isPalindromeSafe(self, head: ListNode) -> bool:
+      nodes = []
+      curr = head
+      
+      while curr:
+         nodes.append(curr.val)
+         curr = curr.next
+         
+      return nodes == nodes[::-1]
